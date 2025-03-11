c7fa65fcd9954736a8d3eb229d7d76d5de59801e
= <aymazhamza@gmail.com>
2025-03-11 13:57:17 +0100

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO, {
+
+    });
+    console.log('MongoDB is connected !!');
+  } catch (error) {
+    console.log(error);
+  }
+}
+ 
+
+
 app.listen(3000, () => {
+  connectDB()
   console.log('Server is running on port 3000 !!');
 })
