b7c6a6003bbd5529979dd65731974c200f22f2f8
= <aymazhamza@gmail.com>
2025-03-11 14:22:38 +0100

    create a test api

--- api/index.js
+++ api/index.js
@@ -2,22 +2,27 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+import UserRouter from './routes/user.route.js';
 const app = express();
+
+app.use(express.json());
+app.use('/api/user', UserRouter)
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO, {
-
+      
     });
     console.log('MongoDB is connected !!');
   } catch (error) {
     console.log(error);
   }
 }
- 
+
 
 
 app.listen(3000, () => {
   connectDB()
   console.log('Server is running on port 3000 !!');
 })
+
