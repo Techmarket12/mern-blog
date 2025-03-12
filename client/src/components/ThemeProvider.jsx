b172b6a2b5de45694470b44e75b2486e78838f0a
= <aymazhamza@gmail.com>
2025-03-12 17:24:40 +0100

    make the dashboard

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -7,7 +7,7 @@ const ThemeProvider = ({children}) => {
 
   return (
     <div className={theme}>
-        <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgba(16,23,42,0.8)] min-h-screen">
+        <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgba(16,23,42)] min-h-screen">
             {children}
         </div>
     </div>
