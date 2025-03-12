d82d6c3888a329b323e1adf5d5e6e325f08745b4
= <aymazhamza@gmail.com>
2025-03-12 07:57:35 +0100

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@
 import User from "../models/user.js";
 import bcryptjs from "bcryptjs";
 import errorHandler from "../utils/errorHandler.js";
+import jwt from "jsonwebtoken";
 export const signup = async(req, res, next) => {
 
    const {username, email, password} = req.body;
@@ -23,4 +24,35 @@ export const signup = async(req, res, next) => {
         
    }
 
+}
+
+export const signin = async(req, res, next) => {
+
+    const { email, password } = req.body;
+
+    if(!email || !password || email === "" || password === "") {
+        next(errorHandler(400, "All fields are required"));
+    }
+
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser) {
+            next(errorHandler(401, "Invalid email or password"));
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+        if(validPassword) {
+            next(errorHandler(401, "Invalid email or password"));
+        }
+
+        const token = jwt.sign( {id: validUser._id}, process.env.JWT_SECRET, {expiresIn: "1h"});
+
+        
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+        }).json(validUser)
+    } catch (error) {
+        next(error);
+    }
 }
