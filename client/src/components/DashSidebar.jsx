1168b29e42fb649b4554c7dc09c22cfe405e9b5f
= <aymazhamza@gmail.com>
2025-03-15 07:30:20 +0100

    show user post inside dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -15,7 +15,6 @@ const DashSidebar = () => {
         const urlParams = new URLSearchParams(location.search)
 
         const tabFromUrl = urlParams.get('tab')
-        console.log(tabFromUrl);
         if(tabFromUrl) {
     
           setTab(tabFromUrl)
