955c0fc7dedd7605c02c4b268b87fcb1ccde0f9e
= <aymazhamza@gmail.com>
2025-03-15 14:16:27 +0100

    create get users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {updateUser, deleteUser, signout } from '../controllers/user.controller.js';
+import {updateUser, deleteUser, signout, getUsers } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/Verifyuser.js';
 
 const UserRouter = express.Router();
@@ -7,5 +7,6 @@ const UserRouter = express.Router();
 UserRouter.put('/update/:userId', verifyToken, updateUser);
 UserRouter.delete('/delete/:userId', verifyToken, deleteUser);
 UserRouter.post('/signout', signout)
+UserRouter.get('/getusers', verifyToken, getUsers)
 
 export default UserRouter;
