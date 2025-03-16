35de8e1c6357eef0a4751b9f6c7f2ca55da18106
= <aymazhamza@gmail.com>
2025-03-16 08:42:59 +0100

    show the comments of a post

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -16,3 +16,15 @@ export const createComment = async (req, res) => {
         next(error)
     }
 }
+
+export const getPostComments = async (req, res) => { 
+    try {
+        const comments = await Comment.find({postId: req.params.postId}).sort({
+            createdAt: -1,
+        })
+        res.status(200).json(comments)
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {updateUser, deleteUser, signout, getUsers } from '../controllers/user.controller.js';
+import {updateUser, deleteUser, signout, getUsers, getUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/Verifyuser.js';
 
 const UserRouter = express.Router();
@@ -8,5 +8,5 @@ UserRouter.put('/update/:userId', verifyToken, updateUser);
 UserRouter.delete('/delete/:userId', verifyToken, deleteUser);
 UserRouter.post('/signout', signout)
 UserRouter.get('/getusers', verifyToken, getUsers)
-
+UserRouter.get('/:userId', verifyToken, getUser)
 export default UserRouter;
