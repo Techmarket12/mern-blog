813fb7d1d8af5af01c7b05fa4380a2910036b423
= <aymazhamza@gmail.com>
2025-03-16 14:58:57 +0100

    add like functionality

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,14 +1,16 @@
 import { Alert, Button, Textarea, TextInput } from 'flowbite-react'
 import React, { useEffect, useState } from 'react'
 import {useSelector} from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import Comment from './Comment.jsx'
+
 import { set } from 'mongoose'
 export default function CommentSection({postId}) {
     const {currentUser} = useSelector(state => state.user)
     const [comment, setComment] = useState('')
     const [commentError, setCommentError] = useState('')
     const [comments, setComments] = useState([])
+    const navigate = useNavigate()
     console.log(comments);
     
     const handleSubmit = async(e) => {
@@ -16,6 +18,7 @@ export default function CommentSection({postId}) {
         
         try {
             if(comment.length > 200 || comment.length < 1) {
+
                 return;
             }
             
@@ -58,6 +61,31 @@ export default function CommentSection({postId}) {
          getComments()
 
     }, [postId])
+    const handleLike = async(commentId) => { 
+        try {
+            if(!currentUser) {
+                navigate('/sign-in')
+                return;
+
+            }
+            const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+                method: 'PUT',
+            })
+            if(res.ok) {
+                const data = await res.json()
+                setComments(comments.map((comment) => 
+                    comment._id === commentId ? {
+                        ...comment,
+                        likes: data.likes,
+                        numberOfLikes: data.likes.length
+                    } : comment
+                ))
+            }
+        } catch (error) {
+            console.log(error.message);
+            
+        }
+    }
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
         {currentUser ? (
@@ -113,7 +141,7 @@ export default function CommentSection({postId}) {
                 </div>
             </div>
             {comments.map(comment => (
-                <Comment key={comment._id} comment={comment}/>
+                <Comment key={comment._id} comment={comment} onLike={handleLike} />
             ))}
             </>
             
