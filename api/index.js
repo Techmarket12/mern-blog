738848738418cb825033a62ff0453adbaf4dd45e
= <aymazhamza@gmail.com>
2025-03-17 20:16:27 +0100

    add pre installation

--- api/index.js
+++ api/index.js
@@ -8,6 +8,10 @@ import postRoutes from './routes/post.route.js';
 import connectDB from './db/connectDB.js';
 import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -20,6 +24,12 @@ app.use('/api/auth', AuthRouter);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500
   const message = err.message || 'Internal Server Error'
@@ -29,8 +39,6 @@ app.use((err, req, res, next) => {
     message
   })
 } )
-
-
 
 
 app.listen(3000, () => {
